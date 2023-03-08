24c022f085b2f8c77c16c5edf5ab9eaf547a5bdf
Ogaga <iyaraogaga69@gmail.com>
2023-03-08 16:20:02 +0100

    Add Styling to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,24 @@
-import React, { FC, useState } from 'react';
-import { GrLocation, GrSearch } from "react-icons/gr";
-import {NavLink} from 'react-router-dom'
-import amazon from './../images/amazon-icon.png'
-import usa from './../images/america-flag.png'
-import styles from './../sass/_navbar.module.scss'
+import React, { FC, useState } from "react";
+import { HiOutlineLocationMarker, HiSearch } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
+import amazon from "./../images/amazon-icon.png";
+import usa from "./../images/america-flag.png";
+import styles from "./../sass/_navbar.module.scss";
+import cart from "./../images/cart.png";
 
 const Navbar: FC = () => {
-    const [showAccountDetails, setShowAccountDetails] = useState<boolean>(false);
+  const [showAccountDetails, setShowAccountDetails] = useState<boolean>(false);
 
-    const handleHover = () => {
-      setShowAccountDetails(true);
-    };
+  const handleHover = () => {
+    setShowAccountDetails(true);
+  };
 
-    const handleLeave = () => {
-      setShowAccountDetails(false);
-    };
+  const handleLeave = () => {
+    setShowAccountDetails(false);
+  };
 
-    return (
+  return (
+    <>
       <section className={styles["nav"]}>
         <div className={styles["nav__image"]}>
           <img
@@ -26,17 +28,17 @@ const Navbar: FC = () => {
           />
         </div>
         <div className={styles["nav__delivery"]}>
-          <GrLocation className={styles["nav__delivery--icon"]} />
+          <HiOutlineLocationMarker className={styles["nav__delivery--icon"]} />
           <div className={styles["nav__para"]}>
             <p className={styles["nav__para--thin"]}>Deliver to</p>
             <br />
-            <p className={styles["nav__para--bold"]}>Nigeria</p>
+            <h1 className={styles["nav__para--bold"]}>Nigeria</h1>
           </div>
         </div>
         <div className={styles["nav__search"]}>
           <input type="text" className={styles["nav__search--input"]} />
           <div className={styles["nav__search--icon"]}>
-            <GrSearch />
+            <HiSearch className={styles["nav__search--search"]} />
           </div>
         </div>
         <div className={styles["nav__language"]}>
@@ -47,55 +49,58 @@ const Navbar: FC = () => {
           />
           <p className={styles["nav__language--name"]}>EN.</p>
         </div>
-        <NavLink to="Account">
-          <div
-            className={styles["nav__account"]}
-            onMouseOver={handleHover}
-            onMouseLeave={handleLeave}
-          >
-            <div className={styles["nav__para"]}>
-              <p className={styles["nav__para--thin"]}>Hello, sign in</p>
-              <br />
-              <p className={styles["nav__para--bold"]}>Account & Lists</p>
-            </div>
-            <div className={styles["nav__triangle"]}></div>
-            {showAccountDetails && (
-              <div className={styles["nav__account--details"]}>
-                <h1 className={styles["nav__account--heading"]}>
-                  Your Account
-                </h1>
-                <ul className={styles["nav__account--list"]}>
-                  <li className={styles["nav__account--item"]}>
-                    <NavLink to="Account">Account</NavLink>
-                  </li>
-                  <li className={styles["nav__account--item"]}>
-                    <NavLink to="Order">Order</NavLink>
-                  </li>
-                  <li className={styles["nav__account--item"]}>
-                    Sign out
-                  </li>
-                </ul>
-              </div>
-            )}
+        <NavLink
+          className={styles["nav__account"]}
+          onMouseOver={handleHover}
+          onMouseLeave={handleLeave}
+          to="Account"
+        >
+          <div className={styles["nav__para"]}>
+            <p className={styles["nav__para--thin"]}>Hello, sign in</p>
+            <br />
+            <h1 className={styles["nav__para--bold"]}>Account & Lists</h1>
+          </div>
+          <div className={styles["nav__triangle"]}></div>
+        </NavLink>
+        <NavLink className={styles["nav__orders"]} to="Orders">
+          <div className={styles["nav__para"]}>
+            <p className={styles["nav__para--thin"]}>Returns</p>
+            <br />
+            <p className={styles["nav__para--bold"]}>& Orders</p>
           </div>
         </NavLink>
-        <NavLink to="Orders">
-          <div className={styles["nav__orders"]}>
-            <div className={styles["nav__para"]}>
-              <p className={styles["nav__para--thin"]}>Returns</p>
-              <br />
-              <p className={styles["nav__para--bold"]}>& Orders</p>
-            </div>
+        <NavLink className={styles["nav__cart"]} to="Cart">
+          <div className={styles["nav__cart--div"]}>
+            <p className={styles["nav__cart--p"]}>0</p>
+            <img
+              src={cart}
+              alt="Amazon Cart"
+              className={styles["nav__cart--img"]}
+            />
           </div>
-        </NavLink>
-        <NavLink to="Cart">
-          <div className={styles["nav__cart"]}>
-            <p className={styles["nav__cart--img"]}>0</p>
-            <p className={styles["nav__cart--para"]}>Cart</p>
-          </div>
+          <p className={styles["nav__cart--para"]}>Cart</p>
         </NavLink>
       </section>
-    );
+      {showAccountDetails && (
+        <div
+          className={styles["nav__account--details"]}
+          onMouseOver={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          <h1 className={styles["nav__account--heading"]}>Your Account</h1>
+          <ul className={styles["nav__account--list"]}>
+            <li className={styles["nav__account--item"]}>
+              <NavLink to="Account" className={styles["nav__account--items"]}>Account</NavLink>
+            </li>
+            <li className={styles["nav__account--item"]}>
+              <NavLink to="Order" className={styles["nav__account--items"]}>Order</NavLink>
+            </li>
+            <li className={styles["nav__account--items"]}>Sign out</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Navbar;
