fbad76e164c093d7c09a2710b2fcd54cad19115b
Ogaga <iyaraogaga69@gmail.com>
2023-03-10 07:50:30 +0100

    Add Mobile Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import {Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import MobileNavbar from './components/MobileNavbar';
 
 const App:FC = () => {
   return (
     <>
       <Navbar />
+      <MobileNavbar/>
       <Routes></Routes>
     </>
   );
