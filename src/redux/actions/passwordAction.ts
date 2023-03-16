4faca2fa8e595adb5c4ebc55f82a914480e81eb6
Ogaga <iyaraogaga69@gmail.com>
2023-03-16 12:25:52 +0100

    Add Authorization to update password request

--- src/redux/actions/passwordAction.ts
+++ src/redux/actions/passwordAction.ts
@@ -10,7 +10,7 @@ export const updatePassword = (
       {
         method: "PATCH",
         headers: {
-          "Authorization": `Bearer ${document.cookie}`,
+          authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
         },
 
