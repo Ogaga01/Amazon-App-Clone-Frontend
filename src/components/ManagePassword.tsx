e77bf30e4fe2866cfaefe3e96e4c99eeeb41b3be
Ogaga <iyaraogaga69@gmail.com>
2023-03-17 11:41:30 +0100

    Make password input visible

--- src/components/ManagePassword.tsx
+++ src/components/ManagePassword.tsx
@@ -1,19 +1,21 @@
 import React, { FC, useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "./../sass/_managepassword.module.scss";
-import { updatePassword } from '../redux/actions/passwordAction';
-import { useAppSelector } from '../redux/index';
+import { updatePassword } from "../redux/actions/passwordAction";
+import { useAppSelector } from "../redux/index";
 import { useNavigate } from "react-router";
 
 const ManagePassword: FC = () => {
   const [password, setPassword] = useState<string>("");
   const [newpassword, setNewPassword] = useState<string>("");
   const [newPasswordConfirm, setNewPasswordConfirm] = useState<string>("");
-  
-  let user = useAppSelector((state) => {
-    return state.loginSlice.user
-  })
+  const [passwordType, setPasswordType] = useState<string>("password");
 
-  const navigate = useNavigate()
+  let user = useAppSelector((state) => {
+    return state.loginSlice.user;
+  });
+
+  const navigate = useNavigate();
 
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
@@ -27,20 +29,28 @@ const ManagePassword: FC = () => {
     setNewPasswordConfirm(e.target.value);
   };
 
+  const handlePasswordType = () => {
+    setPasswordType("text");
+  };
+
+  const resetHandlePasswordType = () => {
+    setPasswordType("password");
+  };
+
   const updateUserPassword = () => {
-    console.log('Update user password')
-    console.log('User:', user)
+    console.log("Update user password");
+    console.log("User:", user);
 
     if (user !== null) {
       const token = user.token;
-        
-      updatePassword(token, password, newpassword, newPasswordConfirm)
+
+      updatePassword(token, password, newpassword, newPasswordConfirm);
     }
 
     user = null;
 
-    navigate('/login');
-  }
+    navigate("/login");
+  };
 
   return (
     <section className={styles["password"]}>
@@ -50,31 +60,50 @@ const ManagePassword: FC = () => {
       <input
         className={styles["password__input"]}
         id="currentPassword"
-        type="password"
+        type={passwordType}
         value={password}
         onChange={handlePassword}
       />
+      {passwordType === "password" ? (
+        <AiOutlineEyeInvisible onClick={handlePasswordType} />
+      ) : (
+        <AiOutlineEye onClick={resetHandlePasswordType} />
+      )}
       <label className={styles["password__label"]} htmlFor="newPassword">
         New Password
       </label>
       <input
         className={styles["password__input"]}
         id="newPassword"
-        type="password"
+        type={passwordType}
         value={newpassword}
         onChange={handleNewPassword}
       />
+      {passwordType === "password" ? (
+        <AiOutlineEyeInvisible onClick={handlePasswordType} />
+      ) : (
+        <AiOutlineEye onClick={resetHandlePasswordType} />
+      )}
       <label className={styles["password__label"]} htmlFor="confirmNewPassword">
         Confirm New Password
       </label>
       <input
         className={styles["password__input"]}
         id="confirmNewPassword"
-        type="password"
+        type={passwordType}
         value={newPasswordConfirm}
         onChange={handleNewPasswordConfirm}
       />
-      <button type="button" className={styles["password__button"]} onClick={updateUserPassword}>
+      {passwordType === "password" ? (
+        <AiOutlineEyeInvisible onClick={handlePasswordType} />
+      ) : (
+        <AiOutlineEye onClick={resetHandlePasswordType} />
+      )}
+      <button
+        type="button"
+        className={styles["password__button"]}
+        onClick={updateUserPassword}
+      >
         Update Password
       </button>
     </section>
